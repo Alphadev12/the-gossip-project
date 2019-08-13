7445ea10fff5b73ac5e8d7572fbc3d576be8eb10
Alpha <ravalasonn@gmail.com>
2019-08-13 16:21:42 +0300

    Restriction de la modification d'un potin et d'un commentaire

--- app/helpers/gossips_helper.rb
+++ app/helpers/gossips_helper.rb
@@ -1,2 +1,9 @@
 module GossipsHelper
+	def user_created_gossip?
+    	@gossip.user == current_user ? true : false
+  	end
+
+  	# def user_created_comment?
+   #  	comment.user == current_user ? true : false
+  	# end
 end
