4907cafd03636a87225a76cef130de8a6312cb5c
Alpha <ravalasonn@gmail.com>
2019-08-13 08:49:54 +0300

    Ajout d'un utilisateur

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,7 +11,24 @@ class UserController < ApplicationController
   end
 
   def create
-    
+    first_name = params['first_name']
+    last_name = params['last_name']
+    age = params['age']
+    email = params['email']
+    description = params['description']
+
+    city_id = params['post']['city_id']
+
+    password = params['password']
+    password_confirm = params['password_confirm']
+
+    user = User.new(first_name: first_name, last_name: last_name, age: age, email: email, description: description, password: password, password_confirmation: password_confirm)
+    city = City.find(city_id)
+
+    user.city = city
+    if user.authenticate(password_confirm)
+      user.save
+    end
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
+	validates :password, 
+	presence: true, 
+	length: { minimum: 6 }
+
 	belongs_to :city
 
 	has_many :gossips
 	has_many :comments
+
+	has_secure_password
 end
